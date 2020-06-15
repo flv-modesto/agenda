146cc95ac91babcce43572e1865b99c4091e71c4
Flavio <fma.modesto@gmail.com>
2020-06-14 21:59:15 -0300

    Adicionado retorno lista json, retorno de erro 404 e acerto timezone

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 # Create your models here.
 
 class Evento(models.Model):
@@ -25,3 +26,10 @@ class Evento(models.Model):
 
     def get_hora_input_evento(self):
         return self.data_evento.strftime('%H:%M')
+
+    def get_evento_atrasado (self):
+        if self.data_evento < datetime.now():
+            return True
+        else:
+            return False
+
